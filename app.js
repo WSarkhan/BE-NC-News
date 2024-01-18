f3e699222f52aa8648bda47008622ee0472d594c
Ward Sarkhan <143825089+WSarkhan@users.noreply.github.com>
2024-01-18 12:05:22 +0000

    Task 8

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/api/articles/:article_id/comments', getCommentsByArticleId)
 
 app.post('/api/articles/:article_id/comments', postComment)
 
-app.patch('/api/articles/:article_id', patchArticle)
+// app.patch('/api/articles/:article_id', patchArticle)
 
 
 app.use(handleCustomErrors);

adb2cbde2881415fa313edc6494752383d0f96a4
Ward Sarkhan <143825089+WSarkhan@users.noreply.github.com>
2024-02-08 15:19:25 +0000

    Update comments.models.js
    Added a sort

--- app.models/comments.models.js
+++ app.models/comments.models.js
@@ -3,7 +3,7 @@ const db = require("../db/connection");
 
 exports.fetchCommentsByArticleId = (article_id) => {
   return db
-    .query("SELECT * FROM comments WHERE article_id = $1;", [article_id])
+    .query("SELECT * FROM comments WHERE article_id = $1 ORDER BY comments.created_at DESC;", [article_id])
     .then(({ rows }) => {
       if (rows.length === 0) {
         return Promise.reject({
